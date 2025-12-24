2f0586dde84d51adad6ea76911cea45b591371cc
Park Jaehyun <earlydreamer@users.noreply.github.com>
2025-12-24 15:15:09 +0900

    0016

--- src/main/java/com/back/global/jpa/entity/BaseEntity.java
+++ src/main/java/com/back/global/jpa/entity/BaseEntity.java
@@ -6,6 +6,8 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
 /**
  * 베이스 엔티티
  */
@@ -13,7 +15,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 // 모든 엔티티들의 조상
-public class BaseEntity {
+public abstract class BaseEntity {
     public String getModelTypeCode() {
         return this.getClass().getSimpleName();
     }
@@ -21,4 +23,13 @@ public class BaseEntity {
     protected void publishEvent(Object event) {
         GlobalConfig.getEventPublisher().publish(event);
     }
+
+    // 이 메소드가 여기 선언되는 이유가 뭐지
+    // 자식 클래스인 BaseIdAndTime에 있어야 하는 거 아닌가
+    // 아직 선언되지 않은(자식에서 선언되는) 필드를 조회하는 메소드가 여기 있는게 맞나
+    // 자식 클래스가 공통으로 사용하는 필드니까 여기서 미리 명세를 해준다고 하는데 적절한 설계가 맞나
+    public abstract int getId();
+    public abstract LocalDateTime getCreatedAt();
+    public abstract LocalDateTime getUpdatedAt();
+
 }
--- src/main/java/com/back/global/jpa/entity/BaseIdAndTimeManual.java
+++ src/main/java/com/back/global/jpa/entity/BaseIdAndTimeManual.java
@@ -1,32 +1,26 @@
 package com.back.global.jpa.entity;
 
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-import static jakarta.persistence.GenerationType.IDENTITY;
-
 /**
- * ID와 시간을 포함하는 기본 엔티티
+ * Auto-Increment와 자동생성을 걸지 않는 베이스 엔티티
  */
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
-public abstract class BaseIdAndTime extends BaseEntity{
+public abstract class BaseIdAndTimeManual extends BaseEntity{
 
     @Id
-    @GeneratedValue(strategy=IDENTITY)
     private int id;
-    @CreatedDate
     private LocalDateTime createdAt;
-    @LastModifiedDate
     private LocalDateTime updatedAt;
+
+
 
 }
